735f103b5e67d8714ca03f8c3a89654a35de7ffc
marblexu <47616581+marblexu@users.noreply.github.com>
2020-01-09 15:27:34 +0800

    Update map.py

--- test1/source/map.py
+++ test1/source/map.py
@@ -4,21 +4,23 @@ from . import constants as c
 
 class Map():
     def __init__(self, width, height, grid):
-        self.width = width  #地图的行数
-        self.height = height #地图的列数
-        #bg_map是地图的背景颜色二维数组，每个元素对应一个地图格子。
+        # 地图的行数
+        self.width = width
+        # 地图的列数
+        self.height = height 
+        # bg_map是地图的背景颜色二维数组，每个元素对应一个地图格子。
         self.bg_map = [[0 for x in range(self.width)] for y in range(self.height)]
         self.setupMapImage(grid)
 
     def setupMapImage(self, grid):
-        #grid_map是地图的格子类型二维数组，每个元素对应一个地图格子
+        # grid_map是地图的格子类型二维数组，每个元素对应一个地图格子
         self.grid_map = [[0 for x in range(self.width)] for y in range(self.height)]
         if grid is not None:
             for data in grid:
                 x, y, type = data['x'], data['y'], data['type']
                 self.grid_map[y][x] = type
         
-        #创建一个和地图一样大小的图片map_image，用来绘制非空的地图格子，比如格子是石块或草地。
+        # 创建一个和地图一样大小的图片map_image，用来绘制非空的地图格子，比如格子是石块或草地。
         self.map_image = pg.Surface((self.width * c.REC_SIZE, self.height * c.REC_SIZE)).convert()
         self.rect = self.map_image.get_rect()
         self.rect.x = 0
@@ -53,7 +55,7 @@ class Map():
             self.bg_map[map_y][map_x] = c.BG_ACTIVE
 
     def drawBackground(self, surface):
-        #根据背景格子类型，设置地图格子为不同的颜色
+        # 根据背景格子类型，设置地图格子为不同的颜色
         for y in range(self.height):
             for x in range(self.width):
                 if self.bg_map[y][x] == c.BG_EMPTY:
@@ -63,19 +65,19 @@ class Map():
                 pg.draw.rect(surface, color, (x * c.REC_SIZE, y * c.REC_SIZE, 
                         c.REC_SIZE, c.REC_SIZE))
         
-        #绘制格子类型的图片
+        # 绘制格子类型的图片
         surface.blit(self.map_image, self.rect)
 
-        #绘制地图每一行的线
+        # 绘制地图每一行的线
         for y in range(self.height):
-            # draw a horizontal line
+            # 根据当前的行值，计算y轴的值为(c.REC_SIZE * y)
             start_pos = (0, 0 + c.REC_SIZE * y)
             end_pos = (c.MAP_WIDTH, c.REC_SIZE * y)
             pg.draw.line(surface, c.BLACK, start_pos, end_pos, 1)
         
-        #绘制地图每一列的线
+        # 绘制地图每一列的线
         for x in range(self.width):
-            # draw a vertical line
+            # 根据当前的列值，计算x轴的值为(c.REC_SIZE * x)
             start_pos = (c.REC_SIZE * x, 0) 
             end_pos = (c.REC_SIZE * x, c.MAP_HEIGHT)
             pg.draw.line(surface, c.BLACK, start_pos, end_pos, 1)
