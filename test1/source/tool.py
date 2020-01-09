a1a247e78dd97dffb35aebbceff3f1a1e09888c2
marblexu <47616581+marblexu@users.noreply.github.com>
2020-01-09 15:19:43 +0800

    Update tool.py

--- test1/source/tool.py
+++ test1/source/tool.py
@@ -1,21 +1,23 @@
-import os  #加载os模块
-import json #加载json模块
-import pygame as pg  #为了书写方便，将pygame缩写为pg
-from . import constants as c #为了书写方便，将constants缩写为c
+import os  # 加载os模块
+import json # 加载json模块
+import pygame as pg  # 为了书写方便，将pygame缩写为pg
+from . import constants as c # 为了书写方便，将constants缩写为c
 from . import level
 
 class Control():
     def __init__(self):
-        #screen会被pygame用来绘制图像
+        # screen会被pygame用来绘制图像
         self.screen = pg.display.get_surface()
-        #done用来判断主循环是否要退出，done为True，游戏结束
-        self.done = False 
+        # done用来判断主循环是否要退出，done为True，游戏结束
+        self.done = False
+        # 生成pygame clock对象
         self.clock = pg.time.Clock()
-        #fps 60表示pygame每秒钟会执行60次
+        # fps 60表示pygame每秒钟会执行的帧数是60
         self.fps = 60
-        #level是游戏运行类
+        # level是游戏运行类
         self.level = level.Level()
-        self.mouse_pos = None #保存鼠标点击时的坐标
+        # 保存鼠标点击时的坐标
+        self.mouse_pos = None
 
     def update(self):
         '''游戏的更新函数'''
@@ -34,20 +36,20 @@ class Control():
                 self.mouse_pos = pg.mouse.get_pos()
 
     def main(self):
+        # 程序主循环
         while not self.done:
-            #获取事件
+            # 获取事件
             self.event_loop()
-            #更新游戏状态
+            # 更新游戏状态
             self.update()
-            #显示图像
+            # 显示图像
             pg.display.update()
-            #等待 1/self.fps 秒时间
+            # 等待 1/self.fps 秒时间
             self.clock.tick(self.fps)
-        print('game over')
 
 def get_image(sheet, x, y, width, height, colorkey, scale):
     '''因为有的大图片里包含了很多小图片，所以要根据传入的坐标参数（x, y, width, height)
-       指定小图片在大图片中的位置，将小图片截取出来，还可以根据参数scale将图片进行放大或缩小。'''
+       指定小图片在大图片中的位置，将小图片截取出来，还可以根据参数scale将图片进行放大或缩小'''
     image = pg.Surface([width, height])
     rect = image.get_rect()
 
@@ -57,17 +59,20 @@ def get_image(sheet, x, y, width, height, colorkey, scale):
     return image
 
 def load_all_gfx(directory, colorkey=c.WHITE, accept=('.png', '.jpg', '.bmp', '.gif')):
-    '''遍历传入参数表示的目录，将所有文件后缀名在accept tuple中的图片都加载到graphics字典中'''
+    '''遍历传入参数表示的目录，将所有文件后缀名在accept元组中的图片都加载到graphics字典中'''
     graphics = {}
+    # 遍历目录中的所有文件
     for pic in os.listdir(directory):
         name, ext = os.path.splitext(pic)
         if ext.lower() in accept:
+            # 文件的后缀名在accept元组中
             img = pg.image.load(os.path.join(directory, pic))
             if img.get_alpha():
                 img = img.convert_alpha()
             else:
                 img = img.convert()
                 img.set_colorkey(colorkey)
+            # 将加载的image保存在graphics字典中，key是文件名
             graphics[name] = img
     return graphics
 
@@ -79,10 +84,14 @@ def load_map_grid_image():
         grid_images[type] = get_image(GFX['tile'], *rect, c.WHITE, 3)
     return grid_images
 
+# pygame的初始化
+pg.init()
+# 设置游戏窗口的标题
+pg.display.set_caption(c.ORIGINAL_CAPTION)
+# 设置游戏窗口的大小（长度和宽度）
+pg.display.set_mode(c.SCREEN_SIZE)
 
-pg.init() #pygame的初始化
-pg.display.set_caption(c.ORIGINAL_CAPTION) #设置游戏窗口的标题
-pg.display.set_mode(c.SCREEN_SIZE) #设置游戏窗口的大小（长度和宽度）
-
+# 加载resources/graphics目录中所有的图片文件
 GFX = load_all_gfx(os.path.join("resources","graphics"))
+# 获取地图格子类型的图片
 GRID = load_map_grid_image()
