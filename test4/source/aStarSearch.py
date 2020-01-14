95c235db335cd87e1493a7973e7fbe8f4f26da7f
marblexu <47616581+marblexu@users.noreply.github.com>
2020-01-14 17:43:49 +0800

    Update aStarSearch.py

--- test4/source/aStarSearch.py
+++ test4/source/aStarSearch.py
@@ -138,10 +138,11 @@ def getAStarDistance(map, source, dest):
 
 def getPath(map, source, dest):
     # 如果找到路径，返回 source 位置到 dest 位置的路径每一步的列表
-    path = []
+    path = None
     location = AStarSearch(map, source, dest)
     if location is not None:
-        # location 节点是 dest 位置
+        path = []
+        # location 节点起始时是 dest 位置
         while location.pre_entry is not None:
             # 按照从 dest 位置反方向添加到路径列表，最后一个是 source 的下一个位置
             path.append(location)
