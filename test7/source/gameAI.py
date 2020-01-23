ee91e07e1c9de2fce73c6b93da204dda9b289d70
marblexu <47616581+marblexu@users.noreply.github.com>
2020-01-23 10:29:59 +0800

    Update gameAI.py

--- test7/source/gameAI.py
+++ test7/source/gameAI.py
@@ -4,11 +4,23 @@ from . import map
 from . import tool, aStarSearch
 
 class EnemyInfo():
-    def __init__(self, enemy):
+    def __init__(self, entity, enemy, location, distance):
+        # 保存敌方生物
         self.enemy = enemy
-        self.range_num = 100
-        self.kill_time = 1000
-        self.remote = False
+        # 保存目的位置
+        enemyinfo.location = location
+        # 保存到目的位置的路径距离
+        enemyinfo.distance = distance
+        # 要经过几轮行动攻击到敌方生物
+        if distance == 0:
+            self.round_num = 0
+        else:
+            self.round_num = (distance - 1) // entity.attr.distance
+        # 敌方生物要攻击几次才会死亡
+        hurt = entity.attr.getHurt(enemy.attr)
+        self.kill_time = (enemy.health-1) // hurt       
+        # 敌方生物是否是远程生物
+        self.remote = enemy.attr.remote
          
 def getAction(entity, map, enemy_group):
     def getDestination(entity, map, enemy):
@@ -22,71 +34,72 @@ def getAction(entity, map, enemy_group):
                 if distance is None:
                     continue
                 if best_pos is None:
-                    best_pos = (x, y)
+                    best_pos = (x, y, distance)
                     min_dis =  distance
                 elif distance < min_dis:
-                    best_pos = (x, y)
+                    best_pos = (x, y, distance)
                     min_dis =  distance
         return best_pos
-
-    def getEnemyInfo(info_list, entity, enemy, destination):
-        enemyinfo = EnemyInfo(enemy)
-        location = aStarSearch.AStarSearch(map, (entity.map_x, entity.map_y), destination)
-        _, _, distance = aStarSearch.getFirstStepAndDistance(location)
-        
-        print('entity(%d, %d), dest(%d, %d) location(%d, %d) distance:%d' % 
-            (entity.map_x, entity.map_y, destination[0], destination[1], location.x, location.y, distance))
-        
-        if distance == 0:
-            enemyinfo.range_num = 0
-        else:
-            enemyinfo.range_num = (distance - 1) // entity.attr.distance
-        enemyinfo.location = location
-        enemyinfo.distance = distance
-        
-        hurt = entity.attr.getHurt(enemy.attr)
-        enemyinfo.kill_time = (enemy.health-1) // hurt
-        
-        enemyinfo.remote = enemy.attr.remote
-        info_list.append(enemyinfo)
     
     info_list = []
     best_info = None
     for enemy in enemy_group:
+        # 遍历敌方生物组中每一个生物，检查生物的地图位置
         if tool.isNextToEntity(entity, enemy):
-            print('entity(%d,%d) next to enemy(%d, %d)' % (entity.map_x, entity.map_y, enemy.map_x, enemy.map_y))
-            destination = (entity.map_x, entity.map_y)
+            # 如果敌方生物在相邻的地图格子，不用移动就可以攻击到
+            location = (entity.map_x, entity.map_y)
+            distance = 0          
         else:
-            destination = getDestination(entity, map, enemy)
-        if destination is not None:
-            getEnemyInfo(info_list, entity, enemy, destination)
+            # 否则找到敌方生物的相邻地图格子行走路径距离最近的格子
+            result = getDestination(entity, map, enemy)
+            if result is None:
+                continue
+            location = (result[0], result[1])
+            distance = result[2]
+       
+        # 创建 EnemyInfo 类对象
+        enemyinfo = EnemyInfo(entity, enemy, location, distance)
+        info_list.append(enemyinfo)
 
+    # 遍历可以攻击到的敌方生物信息列表，按照设定的策略选择最佳的敌方生物
     for info in info_list:
         if best_info == None:
             best_info = info
         else:
-            if info.range_num < best_info.range_num:
+            if info.round_num < best_info.round_num:
+                # 选择攻击前需要行动轮数较小的敌方生物
                 best_info = info
-            elif info.range_num == best_info.range_num:
-                if info.range_num == 0:
+            elif info.round_num == best_info.round_num:
+                # 攻击前需要行动轮数相同时，比较其他属性
+                if info.round_num == 0:
+                    # 如果本轮行动可以攻击到
                     if info.kill_time < best_info.kill_time:
+                        # 选择杀死敌方生物需要的攻击次数最少的
                         best_info = info
                     elif info.kill_time == best_info.kill_time:
+                        # 如果杀死敌方生物需要的攻击次数相同
                         if info.remote == True and best_info.remote == False:
+                            # 优先选择敌方的远程生物
                             best_info = info
                         elif info.distance < best_info.distance:
+                            # 优先选择距离更近的敌方生物
                             best_info = info
                 else:
+                    # 如果至少下一轮行动才能攻击到敌方生物时
                     if info.distance < best_info.distance:
+                        # 优先选择距离更近的敌方生物
                         best_info = info
     
-    if best_info.range_num == 0:
+    if best_info.round_num == 0:
+        # 本轮行动可以攻击到，返回目的位置
         return (best_info.location.x, best_info.location.y, best_info.enemy)
-    elif best_info.range_num == 1:
+    elif best_info.round_num == 1:
+        # 下一轮行动可以攻击到，本轮行走的距离，正好使下一轮能攻击到敌方生物
         range = entity.attr.distance
         x, y = aStarSearch.getPosInRange(best_info.location, range)
         return (x, y, None)
     else:
+        # 至少二轮行动，才能攻击到敌方生物时，本轮行走最大的距离
         range = best_info.distance - entity.attr.distance
         x, y = aStarSearch.getPosInRange(best_info.location, range)
         return (x, y, None)
